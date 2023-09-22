fcdfbe25c2360638bf7d2c515273e569b6968132
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-22 15:13:49 +0630

    add more user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,63 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  Get,
+  Param,
+  NotFoundException,
+  Query,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { UsersService } from './users.service';
 @Controller('auth')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Post('/signup')
   createUser(@Body() body: CreateUserDto) {
+    const { email, password } = body;
+
+    this.userService.create(email, password);
     console.log(body);
+  }
+
+  @Get()
+  async findAll() {
+    return this.userService.find();
+  }
+
+  @Get('/:id')
+  async findUser(@Param('id') id: string) {
+    const user = await this.userService.findOne(parseInt(id));
+
+    if (!user) {
+      throw new NotFoundException('User is not found from auth controller');
+    }
+
+    return user;
+  }
+
+  @Get()
+  async findAllUsersByEmail(@Query('email') email: string) {
+    const user = await this.userService.findByEmail(email);
+
+    if (!user) {
+      throw new NotFoundException('User is not found by email.');
+    }
+
+    return user;
+  }
+
+  @Patch('/:id')
+  async updateUser(@Param('id') id: string, @Body() body: UpdateUserDto ) {
+    return await this.userService.update(parseInt(id), body);
+  }
+
+  @Delete('/:id')
+  async deleteUser(@Param('id') id: string) {
+    return await this.userService.remove(parseInt(id));
   }
 }
