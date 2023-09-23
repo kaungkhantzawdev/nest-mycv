4ff2de7c9205effc009588475461044465a3c3c4
kaungkhantzawjudgify <kaung@judgify.me>
2023-09-23 16:37:56 +0630

    add config

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -2,7 +2,6 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { setupApp } from '../src/setup-app';
 
 describe('Authentication system', () => {
   let app: INestApplication;
@@ -13,12 +12,11 @@ describe('Authentication system', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
-    setupApp(app);
     await app.init();
   });
 
   it('handles signup', () => {
-    const email = 'asdfjkl90@gmail.com';
+    const email = 'asdfjkl92@gmail.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: 'asdf' })
